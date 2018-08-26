2de86aa47fd13fb9304bf9221cafcf34080f2ab3
alpha <827130441@qq.com>
2018-08-26 14:14:35 +0800

    加载测试通过

--- src/components/HomePageBody.vue
+++ src/components/HomePageBody.vue
@@ -2,10 +2,13 @@
   <article class="home-page-main">
     <section class="skeleton-container"></section>
     <section class="collection-container">
-      <CollectCard v-for="collect in collectionsFromServer" :key="collect.full_name" metaData="collect"></CollectCard>
+      <CollectCard v-for="(collect, index) of collectionsFromServer" :key="collect.full_name" :metaData="collect" :index="index" @toTheTop="repositoryToTop" @removeFromCollections="removeFromCollections"></CollectCard>
     </section>
     <section class="starcard-container">
-      <StarCard v-for="star in starsFromServer" :key="star.full_name" metaData="star"></StarCard>
+      <StarCard v-for="(star, index) of filterToShowStars" :key="star.full_name" :metaData="star" :index="index" @addToCollections="addToCollections"></StarCard>
+    </section>
+    <section class="indication">
+      <span v-if="hasLoadedAllStars">—— · No More · ——</span>
     </section>
   </article>
 </template>
@@ -16,11 +19,16 @@ import CollectCard from '@/components/CollectCard'
 
 export default {
   name: 'HomePageMain',
+  props: {
+    userName: {
+      type: String,
+      default: ''
+    }
+  },
   components: { StarCard, CollectCard },
   data: function () {
     return {
-      isLoadedData: false,
-      userName: 'coconilu',
+      hasLoadedAllStars: false,
       collectionsFromLocal: [],
       collectionsFromServer: [],
       starsFromServer: [],
@@ -40,8 +48,8 @@ export default {
     gainUserName: function () {
       this.userName = window.localStorage.getItem('userName') || ''
     },
-    saveUserName: function (value) {
-      window.localStorage.setItem('userName', value)
+    saveUserName: function () {
+      window.localStorage.setItem('userName', this.userName)
     },
     emptyUserName: function () {
       window.localStorage.setItem('userName', '')
@@ -80,36 +88,89 @@ export default {
       }).catch(reason => {
         onFailure && onFailure(reason)
       })
+    },
+    addToCollections: function (index) {
+      this.starsFromServer[index].hasCollected = true
+      this.collectionsFromLocal.unshift(this.starsFromServer[index].full_name)
+      this.collectionsFromServer.unshift(this.starsFromServer[index])
+      setTimeout(this.saveCollections)
+    },
+    repositoryToTop: function (index) {
+      Array.prototype.unshift.apply(this.collectionsFromLocal, this.collectionsFromLocal.splice(index, 1))
+      Array.prototype.unshift.apply(this.collectionsFromServer, this.collectionsFromServer.splice(index, 1))
+      setTimeout(this.saveCollections)
+    },
+    removeFromCollections: function (index) {
+      this.collectionsFromLocal.splice(index, 1)
+      this.collectionsFromServer.splice(index, 1)
+      this.starsFromServer[index].hasCollected = false
+      setTimeout(this.saveCollections)
+    },
+    smoothInsertItem: function (step, targetArr, sourceArr) {
+      var end = 0
+      window.requestAnimationFrame(function insertItem () {
+        targetArr.push.apply(targetArr, sourceArr.slice(end, end + step))
+        end += step
+        if (end < sourceArr.length) {
+          window.requestAnimationFrame(insertItem)
+        }
+      })
+    },
+    loadMoreStars: function () {
+      if (!this.hasLoadedAllStars) {
+        this.getStarsFromServer(this.userName, this.page++, result => {
+          if (result.length < 30) this.hasLoadedAllStars = true
+          this.smoothInsertItem(10, this.starsFromServer, result)
+        })
+      }
     }
   },
   created: function () {
     // this.gainUserName()
+    window.addEventListener('scroll', function () {
+      // auto load more stars
+    })
   },
   watch: {
-    userName: function (newValue) {
-      if (newValue.length) {
-        // login
-        this.gainCollectionsFromLocal()
-        if (this.collectionsFromLocal && this.collectionsFromLocal.length > 0) {
-          this.getCollectionsFromServer(this.collectionsFromLocal, result => {
-            this.collectionsFromServer = result
-          })
-        }
-        this.page = 1
-        this.getStarsFromServer(newValue, this.page++, result => {
-          if (result.length) {
-            Array.prototype.push.apply(this.starsFromServer, result)
-          } else {
-            // TODO to show username unfound
+    userName: {
+      handler: function (newValue) {
+        if (newValue.length) {
+          // login
+          this.gainCollectionsFromLocal()
+          if (this.collectionsFromLocal && this.collectionsFromLocal.length > 0) {
+            this.getCollectionsFromServer(this.collectionsFromLocal, result => {
+              this.smoothInsertItem(10, this.collectionsFromServer, result)
+            })
           }
-        })
-      } else {
-        // logout
-        this.starsFromServer = []
-        this.collectionsFromServer = []
-        this.collectionsFromLocal = []
-        this.emptyCollections()
-      }
+          this.page = 1
+          this.getStarsFromServer(newValue, this.page++, result => {
+            if (result.length >= 0) {
+              // use api requestAnimationFrame
+              this.smoothInsertItem(10, this.starsFromServer, result)
+              if (result.length < 30) this.hasLoadedAllStars = true
+            } else {
+              // TODO: to show username unfounded
+              this.hasLoadedAllStars = true
+            }
+          })
+          setTimeout(this.saveUserName)
+        } else {
+          // logout
+          this.starsFromServer = []
+          this.collectionsFromServer = []
+          this.collectionsFromLocal = []
+          setTimeout(this.emptyCollections)
+          setTimeout(this.emptyUserName)
+        }
+      },
+      immediate: true
+    }
+  },
+  computed: {
+    filterToShowStars: function () {
+      return this.starsFromServer.filter(item => {
+        return !item.hasCollected
+      })
     }
   }
 }
@@ -117,7 +178,9 @@ export default {
 
 <style>
 article.home-page-main {
-  width: 1000px;
+  width: 900px;
+  padding: 0 20px;
+  box-sizing: border-box;
   position: relative;
 }
 section.skeleton-container {
@@ -137,5 +200,14 @@ section.division {
 }
 section.starcard-container {
   position: relative;
+}
+section.indication {
+  line-height: 100%;
+  font-size: 0.8em;
+  opacity: 0.5;
+}
+section.indication > span {
+  vertical-align: middle;
+  font-size: 0.8em;
 }
 </style>
