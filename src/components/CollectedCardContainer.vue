d60d51e8417cb70077710719cd679b20cb2f46ce
alpha <827130441@qq.com>
2018-08-26 22:31:33 +0800

    rebuild structure

---
+++ src/components/CollectedCardContainer.vue
@@ -0,0 +1,17 @@
+<template>
+  <transition-group name="card-container" class="card-container" tag="div" enter-active-class="bounceInRight" leave-active-class="bounceOutDown" move-class="when-move">
+    <slot></slot>
+  </transition-group>
+</template>
+
+<script>
+export default {
+  name: 'CollectedCardContainer'
+}
+</script>
+
+<style>
+.when-move {
+  transition: all 0.8 ease-in-out;
+}
+</style>
---
+++ src/components/StarCardContainer.vue
@@ -0,0 +1,17 @@
+<template>
+  <transition-group name="card-container" class="card-container" tag="div" enter-active-class="bounceInUp" leave-active-class="bounceOutRight" move-class="when-move">
+    <slot></slot>
+  </transition-group>
+</template>
+
+<script>
+export default {
+  name: 'CardContainer'
+}
+</script>
+
+<style>
+.when-move {
+  transition: all 0.8 ease-in-out;
+}
+</style>
