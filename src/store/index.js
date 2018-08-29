070c0163df7365aee3f7bb264527edfc2fb70d89
alpha <827130441@qq.com>
2018-08-29 19:50:47 +0800

    build the vuex module

--- src/store/index.js
+++ src/store/index.js
@@ -5,19 +5,64 @@ Vue.use(Vuex)
 
 export default new Vuex.Store({
   state: {
-    userName: '',
-    avatarURL: '',
+    userName: 'coconilu',
+    avatarURL: 'https://avatars3.githubusercontent.com/u/8131019?v=4',
+    collections: [],
     userInformations: {}
   },
   mutations: {
-    setUserName: function (state, payload) {
+    updateUserName: function (state, payload) {
       state.userName = payload.userName
     },
-    setAvatarURL: function (state, payload) {
+    updateAvatarURL: function (state, payload) {
       state.avatarURL = payload.avatarURL
     },
-    setUserInformations: function (state, payload) {
+    updateUserInformations: function (state, payload) {
       state.userInformations = { ...payload }
+    },
+    updateCollections: function (state, payload) {
+      if (payload.type === 'add') {
+        state.collections.unshift(payload.newItem)
+      } else if (payload.type === 'toTop') {
+        state.collections.unshift.apply(state.collections, state.collections.splice(payload.toTopIndex, 1))
+      } else if (payload.type === 'remove') {
+        state.collections.splice(payload.removeIndex, 1)
+      }
+    },
+    initData: function (state) {
+      // state.userName = window.localStorage.getItem('userName') || ''
+      // state.avatarURL = window.localStorage.getItem('avatarURL') || ''
+      state.collections = JSON.parse(window.localStorage.getItem('collections')) || []
+    },
+    emptyData: function (state) {
+      state.userName = ''
+      state.avatarURL = ''
+      state.collections = []
+      state.userInformations = {}
+    }
+  },
+  actions: {
+    persistUserInformation: function ({ commit, state }, payload) {
+      payload.userName && commit('updateUserName', payload)
+      payload.avatarURL && commit('updateAvatarURL', payload)
+      setTimeout(() => {
+        window.localStorage.setItem('userName', state.userName)
+        window.localStorage.setItem('avatarURL', state.avatarURL)
+      })
+    },
+    persistCollections: function ({ commit, state }, payload) {
+      commit('updateCollections', payload)
+      setTimeout(() => {
+        window.localStorage.setItem('collections', JSON.stringify(state.collections || []))
+      })
+    },
+    logout: function ({ commit, state }) {
+      commit('emptyData')
+      setTimeout(() => {
+        window.localStorage.setItem('userName', state.userName)
+        window.localStorage.setItem('avatarURL', state.avatarURL)
+        window.localStorage.setItem('collections', JSON.stringify(state.collections || []))
+      })
     }
   }
 })
