18b30d13265830ce24b445b235acfad45a4cdd0b
alpha <827130441@qq.com>
2018-09-03 09:52:28 +0800

    rebuild card container

---
+++ src/components/CardContainer.vue
@@ -0,0 +1,20 @@
+<template>
+  <transition-group name="cards-container" class="cards-container" tag="div">
+    <slot></slot>
+  </transition-group>
+</template>
+
+<script>
+export default {
+  name: 'CardContainer'
+}
+</script>
+
+<style>
+div.cards-container{
+  position: relative;
+}
+div.cards-container > .cards-container-move {
+  transition: transform .5s;
+}
+</style>
