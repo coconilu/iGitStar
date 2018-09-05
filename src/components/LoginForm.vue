e65af5873b7a20dd59ed40a9d2b2c2ec3301db55
alpha <827130441@qq.com>
2018-09-05 14:20:03 +0800

    #8 add animation when logining

--- src/components/LoginForm.vue
+++ src/components/LoginForm.vue
@@ -1,6 +1,6 @@
 <template>
   <div class="login-form">
-    <input type="text" placeholder="Your Github Login Name" v-model="userName" @keyup.enter="login"/><button type="submit" @click="login">Login</button>
+    <input type="text" :class="{'input--background-animation': logining}" placeholder="Your Github Login Name" v-model="userName" @keyup.enter="login"/><button type="submit" @click="login">Login</button>
   </div>
 </template>
 
@@ -9,11 +9,13 @@ export default {
   name: 'LoginForm',
   data: function () {
     return {
-      userName: ''
+      userName: '',
+      logining: false
     }
   },
   methods: {
     login: function () {
+      this.logining = true
       this.userName = this.userName.replace(/(^\s*)|(\s*$)/g, '')
       if (this.userName.length) {
         this.$store.dispatch('login', {
@@ -26,29 +28,45 @@ export default {
 </script>
 
 <style>
-div.login-form > * {
+.login-form > * {
   line-height: 1.8em;
   font-size: 1em;
   outline: none;
   border: solid 1px #4d84b2;
 }
-div.login-form > input {
+.login-form > input {
   padding: 0 10px;
   border-radius: 10px 0 0 10px;
 }
-div.login-form > input:focus {
+.login-form > input:focus {
   box-shadow: 0 0 6px rgba(0, 0, 0, 0.2);
 }
-div.login-form > button {
+.input--background-animation {
+  background-image: linear-gradient(to right, #d1d1d1 0, #dbdbdb 100%);
+  background-origin: padding-box;
+  background-size: 0%;
+  background-repeat: no-repeat;
+  background-position: left;
+  animation: input-background-animation 1s linear 0s infinite alternate both;
+}
+.login-form > button {
   padding: 0 6px;
   border-radius: 0 10px 10px 0;
   background-color: #4d84b2;
   color: white;
 }
-div.login-form > button:hover {
+.login-form > button:hover {
   opacity: 0.8;
 }
-div.login-form > button:active {
+.login-form > button:active {
   opacity: 0.6;
+}
+@keyframes input-background-animation {
+  0% {
+    background-size: 0%;
+  }
+  100% {
+    background-size: 100%;
+  }
 }
 </style>
